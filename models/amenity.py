caf82f1b1bb27fd24cd6e3a0f4980fb5666d20f4
Echezona Oji <echyth@yahoo.com>
2024-02-28 13:15:42 +1000

    refactor(task 9): modify classes to use default public class methods

--- models/amenity.py
+++ models/amenity.py
@@ -5,12 +5,4 @@ from base_model import BaseModel
 
 class Amenity(BaseModel):
     """Defines the Amenity Type"""
-
-    def __init__(self, name=""):
-        """Initializer for Amenity instances
-        Attr:
-            name: str
-        """
-        super().__init__()
-        if name:
-            self.name = name
+    name = ""
