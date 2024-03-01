71ba825a9d57fe86c72c56f40bc231de4f69fd85
Zona <echyth@yahoo.com>
2024-03-01 02:20:37 +0100

    refactor(task 5): modify tests to check impact on fewer tests on outcome (#30)

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,83 +1,49 @@
-from pathlib import Path
-import json
+# /usr/bin/python3
+"""This module contains unit tests for the FileStorage class"""
 import unittest
-from unittest.mock import patch
-from models import storage
-from models.base_model import BaseModel
-
-
-def remove_persisted_data(path):
-    """Remove the persisted data file"""
-    try:
-        path.unlink()
-    except FileNotFoundError:
-        pass
-
-
-def get_key(obj):
-    return f"{obj.__class__.__name__}.{obj.id}"
-
-
-storage_path = Path(storage._FileStorage__file_path)
-
+import os
+from models.engine.file_storage import FileStorage
+from utils import class_map
 
 class TestFileStorage(unittest.TestCase):
     """Test cases for the FileStorage class"""
 
-    def setUp(self):
-        storage.objects = {}
-        remove_persisted_data(storage_path)
+    def setUp(self) -> None:
+        self.storage = FileStorage()
+        FileStorage._FileStorage__objects.clear()
 
-    def test_that_storage_starts_without_any_objects_in_memory(self):
-        """Test that FileStorage initializes with an empty objects dict"""
-        self.assertEqual(storage.objects, {})
+    def tearDown(self) -> None:
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_that_storage_creates_database_file_if_necessary(self):
-        """Test that FileStorage has a predefined file path for JSON file"""
-        self.assertTrue(not storage_path.exists())
-        _ = BaseModel()
-        storage.save()
-        self.assertTrue(storage_path.exists())
+    def test_FileStorage_all_method(self):
+        """Tests that the all method of FileStorage works
+        as expected
+        """
+        self.assertEqual(self.storage.all(), {})
+        cls = class_map()["BaseModel"]
+        obj1 = cls()
+        obj2 = cls()
+        obj3 = cls()
+        self.storage.new(obj1)
+        self.storage.new(obj2)
+        self.storage.new(obj3)
+        self.assertEqual(len(self.storage.all()), 3)
+        self.assertTrue(f"{cls.__name__}.{obj1.id}" in self.storage.all())
+        self.assertTrue(f"{cls.__name__}.{obj2.id}" in self.storage.all())
+        self.assertTrue(f"{cls.__name__}.{obj3.id}" in self.storage.all())
 
-    def test_that_storage_can_add_objects_to_memory(self):
-        """Test adding new objects to FileStorage"""
-        with patch.object(storage, 'new', return_value=None):
-            obj = BaseModel()
-            obj_key = get_key(obj)
-        storage.new(obj)
-
-        self.assertEqual(storage.objects[obj_key], obj)
-
-    def test_that_storage_can_save_object_to_file_system(self):
-        """Test persisting objects to the file system"""
-        obj = BaseModel()
-        obj_key = get_key(obj)
-        storage.save()
-
-        with storage_path.open() as database:
-            data = json.load(database)
-
-        self.assertIn(obj_key, data)
-
-    def test_that_storage_can_reload_objects_from_file_system(self):
-        """Test reloading objects from the file system"""
-        obj = BaseModel()
-        obj_key = get_key(obj)
-        storage.save()
-
-        storage.objects = {}
-        storage.reload()
-
-        self.assertEqual(obj.to_dict(),
-                         storage.objects[obj_key].to_dict())
-
-    def test_that_storage_can_show_all_objects(self):
-        """Test that the all method returns all stored objects"""
-        objs = [BaseModel() for _ in range(5)]
-        all_objects = storage.all()
-        self.assertEqual(objs,
-                         list(all_objects.values()))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_FileStorage_reload_method(self):
+        """Tests that the reload method of FileStorage works
+        as expected
+        """
+        cls = class_map()["BaseModel"]
+        obj = cls()
+        self.storage.new(obj)
+        key = f"{type(obj).__name__}.{obj.id}"
+        self.storage.save()
+        self.storage.reload()
+        self.assertIn(key, self.storage.all())
+        self.assertEqual(obj.to_dict(), self.storage.all()[key].to_dict())
