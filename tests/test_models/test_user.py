d4ace033d13c935afbaa7ce63f724aa19affb647
Zona <echyth@yahoo.com>
2024-03-01 03:28:48 +0100

    feature(task 8): implement initial tests for User class (#32)

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""Unit tests for User class"""
+"""Module contains unit tests for User class"""
 import unittest
 from models.user import User
 
 
 class TestUser(unittest.TestCase):
-    pass
+    """Test case for User class"""
+
+    def test_user_instance_instantiation(self):
+        """Tests that a User instance is instantiated
+        with all attributes available
+        """
+        user = User()
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.last_name, "")
